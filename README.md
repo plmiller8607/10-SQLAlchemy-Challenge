13617545059f336fdaf9d50e760f0ce86cd0ec84
plmiller8607 <plmiller8607@gmail.com>
2024-08-26 23:40:21 -0700

    completed climate project

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # 10-SQLAlchemy-Challenge
-Uses SQLAlchemy ORM queries, Pandas, and Matplotlib for analysis and exploration of a climate database
+
+
+Overview
+This project uses SQLAlchemy, Python, Flask, Pandas and Matplotlib for analysis and exploration of Hawaiian temperatures. It is composed of two parts. Part one involves using SQLAlchemy to connect to the SQL file, then performing an exploratory precipitation analysis followed by an exploratory station analysis and create a histogram. Part two uses python to create a Flask API with SQLAlchemy ORM to query the database through various routes
+
+Resources
+Jupyter notebook file
+SQLite file
+CSV files for Hawaiian measurements and stations
+
+
+Usage
+Use SQLAlchemy to create Jupyter Notebook Database Connection
+Create a query that results in a Pandas DataFrame to analyse precipitation
+Create a query that results in a Pandas DataFrame to analyse station data
+Create a Flask API SQLite Connection & Landing Page
+Create Flask API Routes
+
+
+
+Dependencies
+Pandas
+Matplotlib
+Datetime
+SQLAlchemy
+Numpy
+Json
+Flask
+
+
+License
+This project is licensed under the MIT License - see the LICENSE file for details. 
--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 # Import the dependencies.
+import datetime as dt
+import numpy as np
 
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 
 #################################################
@@ -7,24 +13,110 @@
 #################################################
 
 
+engine=create_engine("sqlite:///Resources/hawaii.sqlite")
+
 # reflect an existing database into a new model
+Base=automap_base()
 
 # reflect the tables
+Base.prepare(engine)
 
 
 # Save references to each table
+Measurement=Base.classes.measurement
+Station=Base.classes.station
 
 
 # Create our session (link) from Python to the DB
+session=Session(engine)
+
 
 
 #################################################
 # Flask Setup
 #################################################
 
-
+app=Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    return(
+        f"Welcome to the Hawaii Climate Analysis API<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start<br/>"
+        f"/api/v1.0/temp/start/end<br/>"
+        f"<p>'start' and 'end' date should be in MMDDYYY format.</p>"
+    )
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prv_year=dt.date(2017,8,23) - dt.timedelta(days=365)
+    precipitation=session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prv_year).all()
+    
+    session.close()
+    precip={date:prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+
+    session.close()
+    stations=list(np.ravel(results))
+    return jsonify(stations=stations)
+
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prv_year=dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    results=session.query(Measurement.tobs).\
+    filter(Measurement.station=='USC00519281').\
+    filter(Measurement.date>=prv_year).all()
+
+    session.close()
+    temps=list(np.ravel(results))
+    return jsonify(temps=temps)
+
+
+@app.route("/api/v1.0/temp/<start>")
+
+def start(start=None, end=None):
+    sel=[func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)]
+
+    if not end:
+        start=dt.datetime.strptime(start, "%m%d%Y")
+        results=session.query(*sel).\
+            filter(Measurement.date>=start).all()
+
+    session.close()
+    temps=list(np.ravel(results))
+    return jsonify(temps)
+
+
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def start_end(start=None, end=None):
+    sel=[func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)]
+    start=dt.datetime.strptime(start, "%m%d%Y")
+    end=dt.datetime.strptime(end, "%m%d%Y")
+
+    results=session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+
+    session.close()
+    temps=list(np.ravel(results))
+    return jsonify(temps)
+
+if __name__=="_main_":
+    app.run(debug=True)
+
+
